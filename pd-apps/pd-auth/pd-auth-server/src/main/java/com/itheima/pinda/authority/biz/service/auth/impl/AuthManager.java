dbdfbd490b0cf3c76218fe45c3099844b8edbd9f
TREXA23187 <498111557@qq.com>
2023-02-11 21:54:04 +0000

    feat: generate token when login

--- pd-apps/pd-auth/pd-auth-server/src/main/java/com/itheima/pinda/authority/biz/service/auth/impl/AuthManager.java
+++ pd-apps/pd-auth/pd-auth-server/src/main/java/com/itheima/pinda/authority/biz/service/auth/impl/AuthManager.java
@@ -3,6 +3,9 @@ package com.itheima.pinda.authority.biz.service.auth.impl;
 //处理认证相关
 
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.itheima.pinda.auth.server.utils.JwtTokenServerUtils;
+import com.itheima.pinda.auth.utils.JwtUserInfo;
+import com.itheima.pinda.auth.utils.Token;
 import com.itheima.pinda.authority.biz.service.auth.UserService;
 import com.itheima.pinda.authority.dto.auth.LoginDTO;
 import com.itheima.pinda.authority.dto.auth.UserDTO;
@@ -23,6 +26,9 @@ public class AuthManager {
     @Autowired
     private DozerUtils dozerUtils;
 
+    @Autowired
+    private JwtTokenServerUtils jwtTokenServerUtils;
+
     public R<LoginDTO> login(String account, String password) {
         R<User> userR = check(account, password);
         Boolean isError = userR.getIsError();
@@ -30,11 +36,13 @@ public class AuthManager {
             return R.fail("login fail");
         }
 
-        // 生成jwt令牌
+        User user = userR.getData();
+        Token token = generateUserToken(user);
+
 
         // 缓存用户权限
 
-        LoginDTO loginDTO = LoginDTO.builder().user(dozerUtils.map(userR.getData(), UserDTO.class)).build();
+        LoginDTO loginDTO = LoginDTO.builder().user(dozerUtils.map(userR.getData(), UserDTO.class)).token(token).build();
         return R.success(loginDTO);
     }
 
@@ -48,5 +56,12 @@ public class AuthManager {
         }
 
         return R.success(user);
+    }
+
+
+    // 生成jwt令牌
+    public Token generateUserToken(User user) {
+        JwtUserInfo jwtUserInfo = new JwtUserInfo(user.getId(), user.getAccount(), user.getName(), user.getOrgId(), user.getStationId());
+        return jwtTokenServerUtils.generateUserToken(jwtUserInfo, null);
     }
 }
